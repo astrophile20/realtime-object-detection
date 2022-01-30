6bcf8b4b6ea640b930ed642fed747b2f20d82c66
Ankita Ganguly <87467098+astrophile20@users.noreply.github.com>
2022-01-30 16:42:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+stat = "";
+img = "";
+objects = [];
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("object-status").innerHTML = "Status: Detecting Objects";
+}
+
+function preload() {
+    
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+    if (stat != "") {
+        objectDetector.detect(video, gotResult);
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("object-status").innerHTML = "Status: Object Detected";
+            document.getElementById("number-of-objects-detected").innerHTML = "Number of Objects Detected: " + objects.length;
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    stat = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+}
